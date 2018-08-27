8a97129f04eede6cae421ce6f546c0a8e553a529
Obaida Habboush <habboush@Obaidas-MacBook-Pro.local>
2018-08-27 13:16:43 +0300

    warm up

--- warm-up.sol
+++ warm-up.sol
@@ -1,28 +1,29 @@
-// Fix the errors in this code . 
-
 pragma solidity ^0.4.0;
-
-
 contract exercise {
-
    uint public balance;
-   string  name= "Sara"
+   string  name= "Sara";
    int[] public nums=[1,2,3];
-
-
-   function setBalance(int x) public {
+   function setBalance(uint x) public {
        balance = x;
    }
-
-   function getBalance() public  returns (uint) {
-       return balance;
-   }
-
-
-   function doubleBalance() public view returns (uint) {
-       balance=2*balance;
-       return balance;
-   }
-
    
+   function getBalance() public view returns (uint) {
+       return balance;
+   }
+   
+   function getName() public view returns (string) {
+       return name;
+   }
+   
+    function getInfo() public view returns (string) {
+        return getName();
+    }
+    
+    
+   
+   function doubleBalance() public  returns (uint) {
+       balance = 2 * balance;
+       return balance;
+   }
+
 }
